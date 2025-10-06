56b6923c6555a919036412dacfa871ef07ab9cbe
altinmehmetaj-web <altinmehmetaj@gmail.com>
2025-10-06 21:17:08 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 // Highland Padel & More - Interactive JavaScript
 
+ const mobileMenuBtn = document.getElementById('mobileMenuBtn');
+ const nav = document.querySelector('.nav');
+
   if (mobileMenuBtn && nav) {
     mobileMenuBtn.addEventListener('click', () => {
       nav.classList.toggle('open');
